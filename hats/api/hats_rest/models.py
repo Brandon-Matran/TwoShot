26fcd9c6289ac78c539d80858344bd98f0b528de
Patrick <patmordaunt@gmail.com>
2022-12-02 14:31:55 -0800

    Initial Branch Commit

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unite=True)
+    name = models.CharField(max_length=200)
+
+
+class Hat(models.Model):
+    model = models.CharField(max_length=200)
+    fabric = models.CharField(max_length=200)
+    style_name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    picture_url = models.URLField(null=True)
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.CASCADE,
+    )
